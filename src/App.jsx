6e5ade4ec709c6b9574e338b98ac52c4579e22c2
siddhikharde <siddhikharde2005@gmail.com>
2025-12-25 12:28:57 +0530

    data feched from backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { use } from 'react'
+import { useEffect } from 'react';
+import axios from 'axios';
 
-
+const loadTodos=async ()=>{
+   console.log("Loading todos...");
+   const response=await axios.get("http://localhost:8000/todos");
+    console.log(response);
+}
 function App() {
+  useEffect(()=>{
+    loadTodos();
+  },[]);
   return (
     <div>
-      
+      {
+        
+      }
     </div>
   )
 }
