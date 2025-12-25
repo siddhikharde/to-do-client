e87eb0cbc9980ea67c6ff943c30acbb2be7ea56a
siddhikharde <siddhikharde2005@gmail.com>
2025-12-25 17:56:25 +0530

    to do cards created

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
-import React, { use } from 'react'
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 
-const loadTodos=async ()=>{
+
+
+function App() {
+  const [todos, setTodos]=useState([]);
+  
+  const loadTodos=async ()=>{
    console.log("Loading todos...");
    const response=await axios.get("http://localhost:8000/todos");
-    console.log(response);
+  
+   setTodos(response.data.data);
 }
-function App() {
   useEffect(()=>{
     loadTodos();
   },[]);
   return (
-    <div>
-      {
-        
-      }
+    <div className='bg-[#EAE0CF] min-h-[100vh]' >
+      <h1  className='text-4xl text-[#213448] font-bold text-center p-[20px]'>ToDo list</h1>
+       <div className='flex flex-col justify-center items-center w-full'>
+         {
+          todos.map((todo, index)=>{
+            return(
+              <div key={index} className='w-[90%] md:w-[70%] border-1 px-[20px] py-[15px] m-[10px] rounded-2xl text-2xl bg-[#94B4C1]'>
+              <h3>{todo}</h3>
+            </div>
+            )
+          })
+        }
+       </div>
     </div>
   )
 }
